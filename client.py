55cc1a5bbf9910afab16479bf237ebd3da37065d
blue <ab20082@shibaura-it.ac.jp>
2024-07-15 21:13:42 +0900

    create Tcp client

--- client.py
+++ client.py
@@ -1,2 +1,27 @@
 import socket
+import sys
+from protocol import TCPR
+
+
+class validator:
+    @staticmethod
+    def check_room_name(room_name):
+        pass
+
+    @staticmethod
+    def check_user_name(user_name):
+        pass
+
+    @staticmethod
+    def check_password(password):
+        pass
+
+
+class TcpClient:
+    def __init__(self) -> None:
+        self.socket : socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address: str = "0.0.0.0"
+        self.server_port: int = 8000
+        self.buffer: int = 32
+
 
