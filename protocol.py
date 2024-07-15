cafbb018353560fa323992d77d288aeb19e8b677
blue <ab20082@shibaura-it.ac.jp>
2024-07-15 21:08:35 +0900

    modified protocol and create protocol test

--- protocol.py
+++ protocol.py
@@ -10,7 +10,7 @@ class TCPR:
                    user_name: str,
                    password: str) -> bytes:
 
-        return room_name_size.to_bytes(1, "big") + operation.to_bytes(1, "big") + state.to_bytes(1, "big") + user_name_size.to_bytes(1, "big") + room_name.encode("utf-8") + user_name.encode("utg-8") + password.encode("utf-8")
+        return room_name_size.to_bytes(1, "big") + operation.to_bytes(1, "big") + state.to_bytes(1, "big") + user_name_size.to_bytes(1, "big") + room_name.encode("utf-8") + user_name.encode("utf-8") + password.encode("utf-8")
 
 
     @staticmethod
@@ -31,6 +31,4 @@ class TCPR:
         room_name_end_index: int = 4 + room_name_size
         user_name_end_index: int = room_name_end_index + TCPR.get_user_name_length(header)
 
-        return header[4: room_name_end_index], header[room_name_end_index, user_name_end_index], header[user_name_end_index:]
-        
-
+        return header[4: room_name_end_index].decode("utf-8"), header[room_name_end_index: user_name_end_index].decode("utf-8"), header[user_name_end_index:].decode("utf-8")
--- protocol_test.py
+++ protocol_test.py
@@ -1,7 +1,29 @@
 import unittest
 from protocol import TCPR
 
+class ProtocolTest(unittest.TestCase):
+    def setUp(self):
+        room_name = "hello"
+        operation = 1
+        state = 0
+        user_name = "taichi"
+        password = "Abcv123"
+
+        self.protocol = TCPR.set_header(len(room_name), operation, state, len(user_name), room_name, user_name, password)
+
+    def test_get_operation(self):
+        operation = TCPR.get_operation(self.protocol)
+        self.assertEqual(operation, 1)
+
+    def test_get_state(self):
+        state = TCPR.get_state(self.protocol)
+        self.assertEqual(state, 0)
+
+    def test_get_payload(self):
+        payload = TCPR.get_payload(self.protocol)
+        self.assertEqual(payload, ("hello", "taichi", "Abcv123"))
 
 
-
+if __name__ == "__main__":
+    unittest.main()
 
